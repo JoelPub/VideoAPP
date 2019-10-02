ee083fd4431ee08d75f87b07fb225d69ea4253d1
JoelPub <3195983@qq.com>
2019-10-02 08:02:26 +0800

    Update litemall_schema.sql

--- litemall/litemall-db/sql/litemall_schema.sql
+++ litemall/litemall-db/sql/litemall_schema.sql
@@ -1,5 +1,6 @@
 drop database if exists litemall;
-drop user if exists 'litemall'@'localhost';
+-- MySQL 5.7以上版本支持此语法 drop user if exists 'litemall'@'localhost';
+drop user 'litemall'@'localhost';
 -- 支持emoji：需要mysql数据库参数： character_set_server=utf8mb4
 create database litemall default character set utf8mb4 collate utf8mb4_unicode_ci;
 use litemall;
