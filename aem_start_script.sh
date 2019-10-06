c2b0764e5e32936df68935dc888da557cf6deb0c
joelpub <3195983@qq.com>
2019-10-06 15:18:19 +0800

    start script

--- aem_start_script.sh
+++ aem_start_script.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-echo "running  /home/ec2-user/aem_start_script.sh" 
+echo "running  /home/ec2-user/aem_start_script.sh" >>  /home/ec2-user/serverstart.log 
 #Maintaining the Repository
 #https://docs.adobe.com/content/docs/en/aem/6-1/deploy/platform/storage-elements-in-aem-6.html#Maintaining%20the%20Repository
-echo "gabarge collection" 
+echo "gabarge collection" >>  /home/ec2-user/serverstart.log
 cd /home/ec2-user/AEM/author
 du -sh crx-quickstart/repository/segmentstore
 java -jar oak-run-1.2.7.jar checkpoints crx-quickstart/repository/segmentstore
@@ -12,7 +12,6 @@ java -jar oak-run-1.2.7.jar compact crx-quickstart/repository/segmentstore
 du -sh crx-quickstart/repository/segmentstore 
 
 cd /home/ec2-user/AEM/author/crx-quickstart/bin
-echo "AEM start  /etc/rc.local" 
+echo "AEM start  /etc/rc.local" >>  /home/ec2-user/serverstart.log
 ./start
-
 
--- userpoints_start_script.sh
+++ userpoints_start_script.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-echo "Start up User Points" 
+echo "Start up User Points">>  /home/ec2-user/serverstart.log 
 cd /var/www/html/works/userPoints
 npm run dev
-
