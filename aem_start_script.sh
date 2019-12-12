45532059cb86382da2061b7719bfb5dd31c02609
Joel Wang <joel.wang@gtb.com>
2019-12-12 16:08:23 +0800

    Remove Order

--- aem_start_script.sh
+++ aem_start_script.sh
@@ -4,36 +4,27 @@ echo "running  /home/ec2-user/aem_start_script.sh" >>  /home/ec2-user/serverstar
 #Maintaining the Repository
 #https://docs.adobe.com/content/docs/en/aem/6-1/deploy/platform/storage-elements-in-aem-6.html#Maintaining%20the%20Repository
 echo "gabarge collection" >>  /home/ec2-user/serverstart.log
-#节省内存start
-#cd /home/ec2-user/AEM/author
-#du -sh crx-quickstart/repository/segmentstore
-#java -jar oak-run-1.2.7.jar checkpoints crx-quickstart/repository/segmentstore
-#java -jar oak-run-1.2.7.jar checkpoints crx-quickstart/repository/segmentstore rm-unreferenced
-#java -jar oak-run-1.2.7.jar compact crx-quickstart/repository/segmentstore
-#du -sh crx-quickstart/repository/segmentstore 
-
-#cd /home/ec2-user/AEM/author/crx-quickstart/bin
-#echo "AEM start  /etc/rc.local" >>  /home/ec2-user/serverstart.log
-#./start
-#节省内存end
-
+#gabarge collection start
 cd /home/ec2-user/haidan/AEM/author
 du -sh crx-quickstart/repository/segmentstore
 java -jar oak-run-1.2.7.jar checkpoints crx-quickstart/repository/segmentstore
 java -jar oak-run-1.2.7.jar checkpoints crx-quickstart/repository/segmentstore rm-unreferenced
 java -jar oak-run-1.2.7.jar compact crx-quickstart/repository/segmentstore
 du -sh crx-quickstart/repository/segmentstore 
-
-cd /home/ec2-user/haidan/AEM/author/crx-quickstart/bin
-echo "AEM author start  /etc/rc.local" >>  /home/ec2-user/serverstart.log
-./start
-
 cd /home/ec2-user/haidan/AEM/publish
 du -sh crx-quickstart/repository/segmentstore
 java -jar oak-run-1.2.7.jar checkpoints crx-quickstart/repository/segmentstore
 java -jar oak-run-1.2.7.jar checkpoints crx-quickstart/repository/segmentstore rm-unreferenced
 java -jar oak-run-1.2.7.jar compact crx-quickstart/repository/segmentstore
 du -sh crx-quickstart/repository/segmentstore 
+#gabarge collection end
+
+
+
+cd /home/ec2-user/haidan/AEM/author/crx-quickstart/bin
+echo "AEM author start  /etc/rc.local" >>  /home/ec2-user/serverstart.log
+./start
+
 
 cd /home/ec2-user/haidan/AEM/publish/crx-quickstart/bin
 echo "AEM publish start  /etc/rc.local" >>  /home/ec2-user/serverstart.log
