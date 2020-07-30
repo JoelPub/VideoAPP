7eafe68acb568d810b5b52d693ab5e371e7c4410
Joel Wang <joel.wang@gtb.com>
2020-07-30 19:53:25 +0800

    [APP] scroll play

--- works/video/App.js
+++ works/video/App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React , { useState }from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,6 +14,7 @@ import {
   View,
   Text,
   StatusBar,
+  Dimensions,
 } from 'react-native';
 
 import {
@@ -25,31 +26,64 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 import Video from 'react-native-video';
-import LightVideo from "./one.mp4";
+import LightVideo from "./three.mp4";
 const App: () => React$Node = () => {
+  const [paused, setPaused] = useState(true);
+  const [position, setPostion] = useState({
+    start:null,
+    end:null
+  });
+  const handleVideoLayout = (e)  => {
+    const {height} =  Dimensions.get("window");
+    setPostion({start: e.nativeEvent.layout.y,
+      end:e.nativeEvent.layout.y + height + e.nativeEvent.layout.height });
+  }
+  const handleScroll = (e) => {
+    const scrollPosition = e.nativeEvent.contentOffset.y;
+    if(scrollPosition > position.start && scrollPosition < position.end && paused) {
+      setPaused(false);
+    } else if((scrollPosition < position.start || scrollPosition > position.end) && !paused) {
+      setPaused(true);
+    }
+    console.log(paused?'paused':'playing');
+  }
   return (
     <>
-          <SafeAreaView style={{
-                                flex:1,
-                              }}>
-            <Video source={LightVideo}
-             resizeMode="cover"
-             style={{
-                     position: 'absolute',
-                     top: 0,
-                     left: 0,
-                     bottom: 0,
-                     right: 0
-                   }}/>
+          <SafeAreaView style={styles.container}>
+            <ScrollView scrollEventThrottle={16} onScroll={handleScroll}>
+              <View style={styles.fakeContent}>
+                <Text>{paused?"Paused":"Playing"}</Text>
+              </View>
+              <Video 
+                repeat={false}
+                source={LightVideo}
+                paused={paused}
+                onLayout={handleVideoLayout}
+                resizeMode="cover"
+                style={{width, height:300}}/>
+              <View style={styles.fakeContent}>
+                <Text>{paused?"Paused":"Playing"}</Text>
+              </View>
+            </ScrollView>  
           </SafeAreaView>
     </>
   );
 };
-
+const { height, width } = Dimensions.get("window");
 const styles = StyleSheet.create({
   container: {
     flex:1,
   },
+  fakeContent: {
+    height,
+  },
+  fillWindow : {
+     position: 'absolute',
+     top: 0,
+     left: 0,
+     bottom: 0,
+     right: 0
+   }
 });
 
 export default App;
