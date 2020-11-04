5d2c3584c559e1d1e1ed711760227d9991554e06
Joel Wang <joel.wang@gtb.com>
2020-11-04 18:33:26 +0800

    [VIDEOAPP]SVG animation and tts implemented

--- works/Video-APP/src/modules/home/HomeViewContainer.js
+++ works/Video-APP/src/modules/home/HomeViewContainer.js
@@ -1,7 +1,17 @@
-import { compose, withState } from 'recompose';
+import { compose, withState , lifecycle} from 'recompose';
 
 import HomeScreen from './HomeView';
+import Tts from 'react-native-tts';
 
-export default compose(withState('isExtended', 'setIsExtended', false))(
+export default compose(withState('isExtended', 'setIsExtended', false),
+lifecycle({
+  componentDidMount() {
+    console.log('componentDidMount');
+    Tts.setDefaultLanguage('zh-CN');
+    Tts.speak('圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人');
+    Tts.voices().then(voices => console.log(voices));
+
+  },
+}))(
   HomeScreen,
 );
