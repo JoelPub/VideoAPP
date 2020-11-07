f4125abfe02dfe0b488053b8fb4e95aa3e52379a
JoelPub <3195983@qq.com>
2020-11-07 12:05:17 +0800

    [VIDEOAPP]tts after scroll

--- works/Video-APP/src/modules/home/HomeViewContainer.js
+++ works/Video-APP/src/modules/home/HomeViewContainer.js
@@ -1,15 +1,11 @@
 import { compose, withState , lifecycle} from 'recompose';
 
 import HomeScreen from './HomeView';
-import Tts from 'react-native-tts';
 
 export default compose(withState('isExtended', 'setIsExtended', false),
 lifecycle({
   componentDidMount() {
     console.log('componentDidMount');
-    Tts.setDefaultLanguage('zh-CN');
-    Tts.speak('圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人圣诞老人');
-    Tts.voices().then(voices => console.log(voices));
 
   },
 }))(
