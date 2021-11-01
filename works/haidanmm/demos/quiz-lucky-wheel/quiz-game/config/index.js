c60b65dd03af87fa4850a63d8377b0f60f2a54a2
jwang328 <jwang328@ford.com>
2021-11-01 12:15:56 +0800

    [Demo]quiz-lucky-wheel

--- works/haidanmm/demos/quiz-lucky-wheel/quiz-game/config/index.js
+++ works/haidanmm/demos/quiz-lucky-wheel/quiz-game/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/demos/quiz-lucky-wheel/quiz-game/dist/',
 
     /**
      * Source Maps
