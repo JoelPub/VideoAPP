5dd2bf8899e8fe1efe1f0d9b60e2c5c1760e6d64
JoelPub <fictitiousness@proton.me>
2022-10-16 15:46:45 +0800

    Delete litemall directory

--- litemall/litemall-admin/config/dep.env.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	NODE_ENV: '"production"',
-  ENV_CONFIG: '"dep"',
-  BASE_API: '"https://www.evw.wiki/litemallsl/admin"'
-}
--- litemall/litemall-db/sql/litemall_schema.sql
+++
@@ -1,9 +0,0 @@
-drop database if exists litemall;
--- MySQL 5.7以上版本支持此语法 drop user if exists 'litemall'@'localhost';
-drop user 'litemall'@'localhost';
--- 支持emoji：需要mysql数据库参数： character_set_server=utf8mb4
-create database litemall default character set utf8mb4 collate utf8mb4_unicode_ci;
-use litemall;
-create user 'litemall'@'localhost' identified by '5418none';
-grant all privileges on litemall.* to 'litemall'@'localhost';
-flush privileges;
